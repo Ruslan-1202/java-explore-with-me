6302918af790c169e2809ae48739ac99c3e15cb2
Ruslan Abushaev <rus021281@gmail.com>
2025-05-05 13:20:49 +0300

    Postman 4

--- stats/stats-server/src/main/java/ru/practicum/stats/server/handler/ErrorHandler.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/server/handler/ErrorHandler.java
@@ -45,6 +45,7 @@ public class ErrorHandler {
         log.error(e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
     }
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse exception(final Exception e) {
